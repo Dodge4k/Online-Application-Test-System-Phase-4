af12d41c00878d97bcbb13b3c6ce3f8b8f268ad2
Innocent_Malwane <37239121+Dodge4k@users.noreply.github.com>
2023-11-28 11:47:12 +0200

    Add files via upload

---
+++ Project_Phase4/js/quiz.js
@@ -0,0 +1,55 @@
+let quiz=[
+    {
+        question:"To insert a JavaScript into an HTML page, which tag is used?",
+        option:[
+            "1.<script=javascript>",
+            "2.<javascript>",
+            "3.<script>",
+            "4.<js>",
+        ],
+        answer:3,
+    },
+    {
+        question:" Which of the following is a server-side Java Script object?",
+        option:[
+            "1.File",
+            "2.Function",
+            "3.Date",
+            "4.Variable",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"Which of the following is not a valid JavaScript variable name?",
+        option:[
+            "1.4Java",
+            "2._java",
+            "3.java5",
+            "4.None of the above",   
+        ],
+        answer:1,
+    },
+    {
+        question:"___________ JavaScript is also called client-side JavaScrip",
+        option:[
+            "1.Java",
+            "2.LiveWire ",
+            "3.Navigator",
+            "4.None",   
+        ],
+        answer:3,
+
+    },
+    {
+        question:"What is the alternate name for Java script?",
+        option:[
+            "1.LimeScript",
+            "2.TypeScript",
+            "3.Java",
+            "4.ECMAScript",   
+        ],
+        answer:4,
+    }
+    
+]
